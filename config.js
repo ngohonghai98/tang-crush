721122b5c8d13fb65bc5768b41e7c9cebc730169
ngohonghai98 <86910058+ngohonghai98@users.noreply.github.com>
2022-03-04 13:10:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Như ưi!',
-    introDesc: ` Giận lâu sẽ nhanh già lắm đấy, Như biết điều này chưa nhỉ? Chưa biết à, vậy thì cười lên đi. Như giận Hải bao nhiêu thì Hải nhớ Như bấy nhiêu. Nhắn tin cho Hải nhé`,
+    introTitle: 'Thư à!',
+    introDesc: ` Trái đất vốn lạ thường
+    mà sao bạn cứ đi nhầm đường 
+    lạc vào tim mình lẻ loi
+    đằng sau chữ yêu đấy là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Như đã giận Hải rất nhiều 😙',
-    desc: 'Phải chăng Như sẽ không dỗi nữa ',
-    btnYes: 'Như hổng có dỗi <33',
-    btnNo: 'Không, Hải à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Như dỗi mình Hải <3',
-    btnReply: 'Gửi cho Hải <3',
-    reply: 'thật ra Như hết dỗi Hải rồi <33333333',
-    mess: 'Hải biết mà 🥰. Yêu Như nhiều nhiều 😘😘',
+    title: 'phải chăng bạn đã yêu mình ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'vẫn cứ là thích anh <33',
+    btnNo: 'Không, anh bạn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'yêu thì yêu không yêu thì yêu <33333333',
+    mess: 'mình biết mà 🥰. Yêu bạn Thư nhiều nhiều 😘😘',
     messDesc: 'Tối nay mình nhắn tin cho nhau nha  .',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'dạ, yêu anh <3',
     messLink: 'http://https://www.facebook.com/Haicongtu.me' 
 }
